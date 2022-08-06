d87be9e6f0c52bf0bfa123dcf692f0de3c1be82a
branden-eyk <brandeneyk@gmail.com>
2022-08-06 13:40:54 -0400

    Add comments documenting functions' purpose and how they work

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let computerSelection = getComputerSelection();
 let playerSelection = getPlayerSelection();
-playRound(computerSelection, playerSelection);
 
+//Function for randomly generating the computer's selection of rock, paper, or scissors
 function getComputerSelection(){
-    const number = Math.floor((Math.random() * 3) + 1);
-    switch (number) {
+    const number = Math.floor((Math.random() * 3) + 1); //randomly generates an int between 1 and 3 inclusively
+    switch (number) { //switch case is used to translate random number into a selection for the game
         case 1:
             return "rock";
         case 2:
@@ -17,27 +17,32 @@ function getComputerSelection(){
     }
 }
 
+//Function for getting the player's selection of rock, paper, or scissors
 function getPlayerSelection(){
     let invalidResponse = true;
     let playerSelection = "";
 
-    while (invalidResponse) {
+    while (invalidResponse) { //While loop loops over the following code until the user enters rock, paper, or scissors and nothing else
         playerSelection = prompt("Enter your selection: Rock, Paper, or Scissors").toLowerCase();
-        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"){
-            invalidResponse = false;
+        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"){ //Checks if user entered rock or paper or scissors
+            invalidResponse = false; //makes invalidResponse false and breaks the loop
         } else {
-            alert("Invalid entry. Please only enter Rock, Paper, or Scissors");
+            alert("Invalid entry. Please only enter Rock, Paper, or Scissors"); //alert the user about invalid input so they know why they need to enter again
         }
     }
 
     return playerSelection;
 }
 
+//Function for playing 1 round of Rock Paper Scissors
 function playRound(computerSelection, playerSelection){
+    //Canned responses are setup so that win, lose, and draw responses only need to be written once and can be changed easily
     const winResponse = `You win! ${playerSelection} beats ${computerSelection}`;
     const loseReponse = `You lose. ${playerSelection} beats ${computerSelection}`;
     const drawReponse = `Draw! ${playerSelection} matches ${computerSelection}`;
     
+    //Nested switch cases determine the end result based on what the computer chose and what the player chose
+    //Corresponding canned response is printed to console and a value is returned that can be used to do score keeping logic
     switch (computerSelection){
         case "rock":
             switch (playerSelection){
