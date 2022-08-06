674612d061485331f54860216da84bd28330c3fc
branden-eyk <brandeneyk@gmail.com>
2022-08-06 13:30:24 -0400

    Create funcions for getting player selection and evaluating result

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let computerSelection = getComputerSelection();
-let playerSelection = "";
+let playerSelection = getPlayerSelection();
+playRound(computerSelection, playerSelection);
 
 function getComputerSelection(){
     const number = Math.floor((Math.random() * 3) + 1);
@@ -13,5 +14,70 @@ function getComputerSelection(){
         default:
             console.error("getComputerSelection function generated a number that wasn't between 1 and 3");
             break;
+    }
+}
+
+function getPlayerSelection(){
+    let invalidResponse = true;
+    let playerSelection = "";
+
+    while (invalidResponse) {
+        playerSelection = prompt("Enter your selection: Rock, Paper, or Scissors").toLowerCase();
+        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"){
+            invalidResponse = false;
+        } else {
+            alert("Invalid entry. Please only enter Rock, Paper, or Scissors");
+        }
+    }
+
+    return playerSelection;
+}
+
+function playRound(computerSelection, playerSelection){
+    const winResponse = `You win! ${playerSelection} beats ${computerSelection}`;
+    const loseReponse = `You lose. ${playerSelection} beats ${computerSelection}`;
+    const drawReponse = `Draw! ${playerSelection} matches ${computerSelection}`;
+    
+    switch (computerSelection){
+        case "rock":
+            switch (playerSelection){
+                case "rock":
+                    console.log(drawReponse);
+                    return "draw";
+                case "paper":
+                    console.log(winResponse);
+                    return "win";
+                case "scissors":
+                    console.log(loseReponse);
+                    return "lose";
+            }
+            break;
+        case "paper":
+            switch (playerSelection){
+                case "rock":
+                    console.log(loseReponse);
+                    return "lose";
+                case "paper":
+                    console.log(drawReponse);
+                    return "draw";
+                case "scissors":
+                    console.log(winResponse);
+                    return "win";
+
+            }
+            break;
+        case "scissors":
+            switch (playerSelection){
+                case "rock":
+                    console.log(winResponse);
+                    return "win";
+                case "paper":
+                    console.log(loseReponse);
+                    return "lose";
+                case "scissors":
+                    console.log(drawReponse);
+                    return "draw";    
+            }
+        break;
     }
 }
